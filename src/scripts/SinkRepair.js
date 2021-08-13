89958b062648451acd79fb167573e93ca85e6345
Wade Bradford <wadebradfordtn@gmail.com>
2021-08-13 13:58:44 -0500

    css styling

--- src/scripts/SinkRepair.js
+++ src/scripts/SinkRepair.js
@@ -1,12 +1,12 @@
 import { Requests } from "./Requests.js"
-
+import { ServiceForm } from "./ServiceForm.js"
 
 export const SinkRepair = () => {
     return `
         <h1>Maude and Merle's Sink Repair</h1>
         <section class="serviceForm">
         </section>
-
+            ${ServiceForm()}
         <section class="serviceRequests">
             <h2>Service Requests</h2>
             ${Requests()}
